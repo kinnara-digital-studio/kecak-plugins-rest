83603d4d845956728587ae757d4bb10fb41b4b79
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2016-05-27 14:53:47 +0700

    beta release

--- src/main/java/com/kecak/enterprise/RestOptionBinder.java
+++ src/main/java/com/kecak/enterprise/RestOptionBinder.java
@@ -11,10 +11,14 @@ import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -32,9 +36,9 @@ import org.joget.workflow.model.service.WorkflowManager;
 import org.springframework.context.ApplicationContext;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
-
-import com.kecak.enterprise.commons.Tracer;
+import org.xml.sax.helpers.DefaultHandler;
 
 /**
  *
@@ -109,51 +113,87 @@ public class RestOptionBinder extends org.joget.apps.form.model.FormBinder imple
             HttpResponse response = client.execute(request);
             String responseContentType = response.getEntity().getContentType().getValue();
             
-            BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            
-            StringBuffer sb = new StringBuffer();
-            String line;
-            while((line = br.readLine()) != null) {
-            	sb.append(line);
-            }
-            
-            System.out.println(sb.toString());
-            
             if(responseContentType.contains("application/xml") || responseContentType.contains("text/xml")) {
 				try {
-					DocumentBuilder builder = DocumentBuilderFactory.newInstance()
-					        .newDocumentBuilder();
+					String recordPath = getPropertyString("recordPath");
+					String valuePath = getPropertyString("valuePath");
+					String labelPath = getPropertyString("labelPath");
 					
-					// parse document from response input stream
-					Document doc = builder.parse(response.getEntity().getContent());
+					FormRowSet result = new FormRowSet();
+					SAXParserFactory factory = SAXParserFactory.newInstance();
+					SAXParser saxParser = factory.newSAXParser();
+					saxParser.parse(response.getEntity().getContent(),
+							new XmlSaxHandler(
+									Pattern.compile(recordPath.replaceAll("\\.", "\\.") + "$", Pattern.CASE_INSENSITIVE),
+									Pattern.compile(valuePath.replaceAll("\\.", "\\.") + "$", Pattern.CASE_INSENSITIVE),
+									Pattern.compile(labelPath.replaceAll("\\.", "\\.") + "$", Pattern.CASE_INSENSITIVE),
+									result
+							));
 					
-					// normalize document
-					doc.getDocumentElement().normalize();
-					
-					// get root node
-					Node rootNode = doc.getDocumentElement();
-										
-					String[] valueVariables = getPropertyString("valuePath").split("\\.");
-					String[] labelVariables = getPropertyString("labelPath").split("\\.");
-					
-					Node currentNode = rootNode;
-					for(String rowVariable : rowVariables) {
-						if(currentNode == null)
-							break;
-						currentNode = Tracer.xmlFindChild(getPropertyString("rowPath"), currentNode);
-					}
-					
-					
+					System.out.println("result");
+					for(FormRow row : result) {
+						System.out.println("row");
+						for(Map.Entry entry : row.entrySet()) {
+							System.out.println(entry.getKey().toString() + "=>" + entry.getValue().toString());
+						}
+					}					
+
+					return result;
+				} catch (UnsupportedOperationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (SAXException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
 				} catch (ParserConfigurationException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} catch (UnsupportedOperationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (SAXException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
 				}
+				
+//				try {
+//					DocumentBuilder builder = DocumentBuilderFactory.newInstance()
+//					        .newDocumentBuilder();
+//					
+//					// parse document from response input stream
+//					Document doc = builder.parse(response.getEntity().getContent());
+//					
+//					// normalize document
+//					doc.getDocumentElement().normalize();
+//					
+//					// get root node
+//					Node rootNode = doc.getDocumentElement();
+//					
+//					FormRowSet result = new FormRowSet();
+//					xmlTrace("", rootNode, getPropertyString("recordPath"), getPropertyString("valuePath"), getPropertyString("labelPath"), result);
+//					
+//					System.out.println("result");
+//					for(FormRow row : result) {
+//						System.out.println("row");
+//						for(Map.Entry entry : row.entrySet()) {
+//							System.out.println(entry.getKey().toString() + "=>" + entry.getValue().toString());
+//						}
+//					}					
+//					return result;
+//					
+//				} catch (ParserConfigurationException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (UnsupportedOperationException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (SAXException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+            } else {
+            	BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                
+                StringBuffer sb = new StringBuffer();
+                String line;
+                while((line = br.readLine()) != null) {
+                	sb.append(line);
+                }
+                System.out.println(sb.toString());
             }
             
         } catch (IOException ex) {
@@ -161,4 +201,137 @@ public class RestOptionBinder extends org.joget.apps.form.model.FormBinder imple
         }
         return null;
     }   
+    
+    private void xmlTrace(String currentPath, Node currentNode, String recordPath, String valuePath, String labelPath, FormRowSet rowSet) {
+    	if(currentNode != null && currentNode.getNodeType() == Node.ELEMENT_NODE) {
+    		org.w3c.dom.Element element = (org.w3c.dom.Element) currentNode;
+    		System.out.println("element : " + element.getTagName());
+    		currentPath += (currentPath.isEmpty() ? "" : ".") + currentNode.getNodeName();
+    		System.out.println("++++++++++++++++++++++++++++");
+			System.out.println("current record path : " + currentPath);
+			System.out.println("record pattern : " + recordPath.replaceAll("\\.", "\\."));
+			System.out.println("++++++++++++++++++++++++++++");
+			Pattern recordPattern = Pattern.compile(recordPath.replaceAll("\\.", "\\.") + "$", Pattern.CASE_INSENSITIVE);
+			Matcher matcher = recordPattern.matcher(currentPath);
+			if(matcher.find()) {
+				// trace children for value and label
+	    		for(int i = 0, size = currentNode.getChildNodes().getLength(); i < size; i++) {
+	    			Node childNode = currentNode.getChildNodes().item(i);
+	    			FormRow row = new FormRow();
+	    			if(xmlTraceValue("", childNode, "value", valuePath, row) && xmlTraceValue("", childNode, "label", labelPath, row))
+	    				rowSet.add(row);
+	    		}
+			} else {
+				// trace children for record
+	    		for(int i = 0, size = currentNode.getChildNodes().getLength(); i < size; i++) {
+	    			Node childNode = currentNode.getChildNodes().item(i);
+	    			System.out.println("trace child record");
+	    			xmlTrace(currentPath, childNode, recordPath, valuePath, labelPath, rowSet);
+	    		}
+			}	
+    	}
+	}
+    
+    private boolean xmlTraceValue(String path, Node currentNode, String label, String keyPath, FormRow row) {
+    	if(currentNode != null) {
+	    	path += (path.isEmpty() ? "" : ".") + currentNode.getNodeName();
+	    	System.out.println("========================");
+	    	System.out.println("keyPath : " + keyPath);
+	    	System.out.println("current " + label + " path : " + path);
+	    	System.out.println(label + " pattern : " + keyPath.replaceAll("\\.", "\\."));
+	    	System.out.println("========================");
+	    	Pattern valuePattern = Pattern.compile(keyPath.replaceAll("\\.", "\\.") + "$", Pattern.CASE_INSENSITIVE);
+	    	Matcher matcher = valuePattern.matcher(path);
+	    	
+	    	if(matcher.find() && currentNode.getNodeType() == Node.ELEMENT_NODE) {
+	    		org.w3c.dom.Element element = (org.w3c.dom.Element)currentNode;
+	    		System.out.println("element.getNodeValue() : " + element.getNodeValue());
+	    		row.setProperty(label, currentNode.getNodeValue());
+	    		return true;
+	    	} else if (currentNode.getChildNodes() != null){
+	    		// trace children
+	    		for(int i = 0, size = currentNode.getChildNodes().getLength(); i < size; i++) {
+	    			Node childNode = currentNode.getChildNodes().item(i);
+	    			boolean found = xmlTraceValue(path, childNode, label, keyPath, row);
+	    			if(found)
+	    				return true;
+	    		}
+	    	}
+    	}
+    	
+    	return false;
+    }
+    
+    private static class XmlSaxHandler extends DefaultHandler {
+    	private final static String VALUE_KEY = "value";
+    	private final static String LABEL_KEY = "label";
+    	
+    	private String currentPath = "";
+    	private FormRowSet rowSet;
+    	private FormRow row;
+    	private Pattern recordPattern;
+    	private Pattern valuePattern;
+    	private Pattern labelPattern;
+    	
+    	public XmlSaxHandler(Pattern recordPattern, Pattern valuePattern, Pattern labelPattern, FormRowSet rowSet) {
+    		this.recordPattern = recordPattern;
+    		this.valuePattern = valuePattern;
+    		this.labelPattern = labelPattern;
+    		this.rowSet = rowSet;
+    		row = null;
+    	}
+    	
+    	@Override
+    	public void startElement(String uri, String localName, String qName, Attributes attributes)
+    			throws SAXException {
+    		currentPath += (currentPath.isEmpty() ? "" : ".") + qName;
+    		Matcher m = recordPattern.matcher(currentPath);
+    		if(m.find()) {
+    			row = new FormRow();
+    		}
+    		
+    		System.out.println("IN : currentPath");
+    	}
+    	
+    	@Override
+    	public void characters(char[] ch, int start, int length) throws SAXException {
+    		String content = new String(ch, start, length).trim();
+    		System.out.println("currentPath : " + currentPath);
+			System.out.println("content : " + content);
+			
+			if(row != null) {
+				Matcher valueMatcher = valuePattern.matcher(currentPath);
+				Matcher labelMatcher = labelPattern.matcher(currentPath);
+				if(valueMatcher.find()) {
+					row.setProperty(VALUE_KEY, content);
+				} else if(labelMatcher.find()) {
+					row.setProperty(LABEL_KEY, content);
+				}
+			}
+    	}
+    	
+    	@Override
+    	public void endElement(String uri, String localName, String qName) throws SAXException {
+    		System.out.println("OUT : currentPath");
+    		
+    		Matcher m = recordPattern.matcher(currentPath);
+    		if(m.find()) {
+    			System.out.println("1");
+    			if(row != null) {
+    				System.out.println("2");
+    				System.out.println("row.getProperty(VALUE_KEY) : " + row.getProperty(VALUE_KEY));
+    				System.out.println("row.getProperty(LABEL_KEY) : " +  row.getProperty(LABEL_KEY));
+    				
+	    			if(row.getProperty(VALUE_KEY) != null && row.getProperty(LABEL_KEY) != null ) {
+	    				System.out.println("3");
+	    				rowSet.add(row);
+	    			}
+	    			
+	    			row = null;
+    			}
+    		}
+    		
+    		currentPath = currentPath.replaceAll("(\\." + qName + "$)|(^" + qName + "$)", "");
+    	}
+    }
 }
